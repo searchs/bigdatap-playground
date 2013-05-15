57295bc4db20d768b04cd63b2c4c6d999a9a3fae
Ola Ajibode <ola.ajibode@gmail.com>
2013-05-15 15:31:53 +0200

    Checking java nature on github

---
+++ frush.java
@@ -0,0 +1,26 @@
+import junit.org.*;
+
+public class DataMart {
+  
+ private String something;
+ private int counted;
+ public boolean theTruth = false;
+ 
+ 
+ public static void runna(){
+   
+   
+   System.out.println("Checking Data flows");
+   
+ }
+  
+  public static void main(String[] agrs) {
+    
+    system.out.println("No prints please.  this is just a small casing test! ");
+    
+    
+  }
+  
+  
+  
+}
