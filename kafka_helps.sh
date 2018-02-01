64d2e1dd9453e59e476a9433fc19d0083b8c77d6
Ola Ajibode <searchs@users.noreply.github.com>
2018-02-01 16:03:13 +0000

    Create kafka_helps for easy kafka operations

---
+++ kafka_helps.sh
@@ -0,0 +1,30 @@
+# Starting ZooKeeper
+# Local Instance
+bin/zookeeper-server-start.sh config/zookeeper.properties 
+
+# ZooKeeper Config: broker-single node cluster 
+# the directory where the snapshot is stored.
+dataDir=/tmp/zookeeper
+# the port at which the clients will connect
+clientPort=2181
+# disable the per-ip limit on the number of connections since this is a non-production config
+maxClientCnxns=0
+
+# multiple brokers on a single node, different server property files are required for each broker.
+# configure: broker.id, port, log.dir
+# Start each broker with its config
+bin/kafka-server-start.sh config/server_1.properties
+
+# Create Kafka topic
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partition 1 --topic replica-kafkatopic
+
+
+# Get list of topics
+bin/kafka-topics.sh --list --zookeeper localhost:2181
+
+
+# Start a Kafka Producer
+$ bin/kafka-console-producer.sh --broker-list localhost:9092  --topic your-kafkatopic
+
+# Start a Kafka Consumer
+$ bin/kafka-console-consumer.sh --zookeeper localhost:2181 --from-beginning --topic your-kafkatopic
